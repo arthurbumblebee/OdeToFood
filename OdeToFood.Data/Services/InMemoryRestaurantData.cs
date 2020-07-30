047a55d58b65efd03af1118dd8915b22dc694e4e
Arthur Makumbi <Arthur.makumbi@logicinfo.com>
2020-07-30 17:19:22 -0500

    revet "merging"
    This reverts commit 9b0216f79d5f025cbaf61d1fc2d1db87efa622d6, reversing
    changes made to dce9cfb9c4c2d3314e8473ba6dc722771e5f1e75.

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,29 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        { 
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id=1, Name="Arthur's Pizza", Cuisine=CuisineType.Italian},
+                new Restaurant{Id=2, Name="Makumbi's Paris Delight", Cuisine=CuisineType.French},
+                new Restaurant{Id=2, Name="Yawe's Chinatown", Cuisine=CuisineType.China},
+
+            };
+
+        }
+
+        public IEnumerable<Restaurant> GetRestaurants()
+        {
+            return restaurants.OrderBy(restaurants => restaurants.Name);
+        }
+    }
+}
