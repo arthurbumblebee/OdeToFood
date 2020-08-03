f001cf11f233cd3f10cb4ca35ca4a6666460aacf
Arthur Makumbi <Arthur.makumbi@logicinfo.com>
2020-08-03 14:24:47 -0500

    passing data across requests using the TempData structure

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -81,6 +81,7 @@ namespace OdeToFood.Web.Controllers
             if (ModelState.IsValid)
             {
                 database.UpdateRestaurant(restaurant);
+                TempData["Message"] = "You have saved the restaurant!";
                 return RedirectToAction("Details", new { id=restaurant.Id });
             }
             return View(restaurant);
